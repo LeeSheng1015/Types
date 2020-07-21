adb77fbbd80273e19115f723c0cfd4d6c0aac8ed
LeeSheng <727151354@qq.com>
2020-07-21 23:33:05 +0800

    Initial Commit

---
+++ Types/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Types
+//
+//  Created by Lee Sheng on 2020/7/21.
+//  Copyright © 2020 Lee Sheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
